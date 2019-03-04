d674618541b3a76c948913243d8177655574037a
hiroki19990625 <hiroki19990625@gmail.com>
2019-03-04 22:03:10 +0900

    Versionを追加。

--- EngineGL/Properties/AssemblyInfo.cs
+++ EngineGL/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("EngineGL")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("hiroki")]
 [assembly: AssemblyProduct("EngineGL")]
 [assembly: AssemblyCopyright("Copyright © hiroki 2019")]
 [assembly: AssemblyTrademark("")]
@@ -31,5 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.*")]
