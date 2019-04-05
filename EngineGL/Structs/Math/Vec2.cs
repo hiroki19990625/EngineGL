93e35ed2ceddba04622f4c0053a793309f6602b1
hiroki19990625 <hiroki19990625@gmail.com>
2019-04-05 23:47:41 +0900

    Vec2の利便性を向上

--- EngineGL/Structs/Math/Vec2.cs
+++ EngineGL/Structs/Math/Vec2.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Drawing;
 using System.Runtime.CompilerServices;
 using Assimp;
 using OpenTK;
@@ -10,12 +11,12 @@ namespace EngineGL.Structs.Math
     {
         public static Vec2 Zero = new Vec2(0f, 0f);
         public static Vec2 One = new Vec2(1f, 1f);
-        
+
         public static Vec2 Up = new Vec2(0f, 1f);
         public static Vec2 Down = new Vec2(0f, -1f);
         public static Vec2 Right = new Vec2(1f, 0f);
         public static Vec2 Left = new Vec2(-1f, 0f);
-        
+
         public float X { get; set; }
         public float Y { get; set; }
 
@@ -56,6 +57,11 @@ namespace EngineGL.Structs.Math
             {
                 return (X.GetHashCode() * 397) ^ Y.GetHashCode();
             }
+        }
+
+        public override string ToString()
+        {
+            return $"EngineGL.Structs.Math.Vec2: <X: {X}, Y: {Y}>";
         }
 
         public static Vec2 operator +(Vec2 a, Vec2 b)
@@ -113,7 +119,17 @@ namespace EngineGL.Structs.Math
             return new Vec2(a.X, a.Y);
         }
 
+        public static explicit operator Vec2(Vector3 a)
+        {
+            return new Vec2(a.X, a.Y);
+        }
+
         public static implicit operator Vec2(System.Numerics.Vector2 a)
+        {
+            return new Vec2(a.X, a.Y);
+        }
+
+        public static explicit operator Vec2(System.Numerics.Vector3 a)
         {
             return new Vec2(a.X, a.Y);
         }
@@ -121,6 +137,21 @@ namespace EngineGL.Structs.Math
         public static implicit operator Vec2(Vector2D a)
         {
             return new Vec2(a.X, a.Y);
+        }
+
+        public static explicit operator Vec2(Vector3D a)
+        {
+            return new Vec2(a.X, a.Y);
+        }
+
+        public static implicit operator Vec2(PointF a)
+        {
+            return new Vec2(a.X, a.Y);
+        }
+
+        public static implicit operator Vec2(SizeF a)
+        {
+            return new Vec2(a.Width, a.Height);
         }
 
         public static explicit operator Vec2(Vec3 a)
@@ -141,6 +172,16 @@ namespace EngineGL.Structs.Math
         public static implicit operator Vector2D(Vec2 a)
         {
             return new Vector2D(a.X, a.Y);
+        }
+
+        public static implicit operator PointF(Vec2 a)
+        {
+            return new PointF(a.X, a.Y);
+        }
+
+        public static implicit operator SizeF(Vec2 a)
+        {
+            return new SizeF(a.X, a.Y);
         }
     }
 }
