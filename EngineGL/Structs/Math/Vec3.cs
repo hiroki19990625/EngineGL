5a4011d48b1cd17c9d9eeb06d6f732f6693cf573
hiroki19990625 <hiroki19990625@gmail.com>
2019-04-19 00:09:37 +0900

    Vec3の利便性を向上 ファイル追加し忘れて

--- EngineGL/Structs/Math/Vec3.cs
+++ EngineGL/Structs/Math/Vec3.cs
@@ -69,6 +69,11 @@ namespace EngineGL.Structs.Math
             }
         }
 
+        public override string ToString()
+        {
+            return $"EngineGL.Structs.Math.Vec3: <X: {X}, Y: {Y}, Z: {Z}>";
+        }
+
         public static Vec3 operator +(Vec3 a, Vec3 b)
         {
             return new Vec3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
