8aa7d6af13f174dbd3b13f34586e6d448f3df767
hiroki19990625 <hiroki19990625@gmail.com>
2019-05-24 13:59:38 +0900

    ドキュメント作成を更新

--- EngineGL/Core/IComponentAttachable.cs
+++ EngineGL/Core/IComponentAttachable.cs
@@ -23,9 +23,8 @@ namespace EngineGL.Core
         /// アタッチされているコンポーネントを全て取得します。
         /// </summary>
         /// <returns>
-        /// 成功した場合、<see cref="Result{T}"/>でラップされた
-        /// アタッチされている全てのコンポーネントを返します。
-        /// 失敗した場合、<see cref="Result{T}"/> の Fail を返します。
+        /// アタッチされているコンポーネントを全て返します。
+        /// <para>成功または、失敗する可能性があります。</para>
         /// </returns>
         Result<IComponent[]> GetComponents();
 
@@ -34,39 +33,162 @@ namespace EngineGL.Core
         /// </summary>
         /// <param name="hash">インスタンスID</param>
         /// <returns>
-        /// 成功した場合、<see cref="Result{T}"/>でラップされた
-        /// インスタンスID がら取得したコンポーネントを返します。
-        /// 失敗した場合、<see cref="Result{T}"/> の Fail を返します。
+        ///　アタッチされているコンポーネント返します。
+        /// <para>成功または、失敗する可能性があります。</para>
         /// </returns>
         Result<IComponent> GetComponent(Guid hash);
 
         /// <summary>
-        /// 型情報からアタッチされているコンポーネントを取得します。
-        /// この操作は安全ではありません。
-        /// 型情報の型に明示的キャストを行うため例外発生する可能性があります。
-        /// また、複数のコンポーネントが存在する場合、最初に見つかったコンポーネントを返すため、
-        /// 個々のコンポーネントを取得する場合、<seealso cref="GetComponentUnsafe{T}(Guid)"/>
-        /// メソッドを利用してください。
+        /// ジェネリクス型情報からアタッチされているコンポーネントを取得します。
         /// </summary>
         /// <typeparam name="T"><see cref="IComponent"/>型の型引数</typeparam>
-        /// <returns></returns>
+        /// <returns>
+        ///　取得したコンポーネントを返します。
+        /// <para>成功または、失敗する可能性があります。</para>
+        /// </returns>
         Result<T> GetComponentUnsafe<T>() where T : IComponent;
 
+        /// <summary>
+        /// ジェネリクス型情報とインスタンスID からアタッチされているコンポーネントを取得します。
+        /// </summary>
+        /// <param name="hash">インスタンスID</param>
+        /// <typeparam name="T"><see cref="IComponent"/>型の型引数</typeparam>
+        /// <returns>成功または、失敗する可能性があります。</returns>
         Result<T> GetComponentUnsafe<T>(Guid hash) where T : IComponent;
+
+        /// <summary>
+        /// 型情報からアタッチされているコンポーネントを取得します。
+        /// </summary>
+        /// <param name="type">型情報</param>
+        /// <returns>
+        ///　取得したコンポーネントを返します。
+        /// <para>成功または、失敗する可能性があります。</para>
+        /// </returns>
         Result<IComponent> GetComponentUnsafe(Type type);
+
+        /// <summary>
+        /// 型情報とインスタンスIDからアタッチされているコンポーネントを取得します。
+        /// </summary>
+        /// <param name="type">型情報</param>
+        /// <param name="hash">インスタンスID</param>
+        /// <returns>
+        ///　取得したコンポーネントを返します。
+        /// <para>成功または、失敗する可能性があります。</para>
+        /// </returns>
         Result<IComponent> GetComponentUnsafe(Type type, Guid hash);
 
+        /// <summary>
+        /// コンポーネントをアタッチします。
+        /// </summary>
+        /// <param name="component">アタッチするコンポーネント</param>
+        /// <returns>
+        /// アタッチしたコンポーネントを返します。
+        /// <para>成功または、失敗する可能性があります。</para>
+        /// </returns>
         Result<IComponent> AddComponent(IComponent component);
+
+        /// <summary>
+        /// コンポーネントをアタッチします。
+        /// </summary>
+        /// <param name="component">アタッチするコンポーネント</param>
+        /// <typeparam name="T"><see cref="IComponent"/>型の型引数</typeparam>
+        /// <returns>
+        ///　アタッチしたコンポーネントを返します。
+        /// <para>成功または、失敗する可能性があります。</para>
+        /// </returns>
         Result<T> AddComponentUnsafe<T>(T component) where T : IComponent;
+
+        /// <summary>
+        /// ジェネリクス型情報を基にインスタンスを作成してコンポーネントをアタッチします。
+        /// </summary>
+        /// <typeparam name="T"><see cref="IComponent"/>型の型引数</typeparam>
+        /// <returns>
+        ///　アタッチしたコンポーネントを返します。
+        /// <para>成功または、失敗する可能性があります。</para>
+        /// </returns>
         Result<T> AddComponentUnsafe<T>() where T : IComponent;
+
+        /// <summary>
+        /// 型情報を基にインスタンスを作成してコンポーネントをアタッチします。
+        /// </summary>
+        /// <param name="type">型情報</param>
+        /// <returns>
+        ///　アタッチしたコンポーネントを返します。
+        /// <para>成功または、失敗する可能性があります。</para>
+        /// </returns>
         Result<IComponent> AddComponentUnsafe(Type type);
 
+        /// <summary>
+        /// インスタンスID からアタッチされているコンポーネントを削除します。
+        /// </summary>
+        /// <param name="hash"></param>
+        /// <returns>
+        ///　削除されたコンポーネントを返します。
+        /// <para>成功または、失敗する可能性があります。</para>
+        /// </returns>
         Result<IComponent> RemoveComponent(Guid hash);
+
+        /// <summary>
+        /// インスタンスからアタッチされているコンポーネントを削除します。
+        /// </summary>
+        /// <param name="component">コンポーネントのインスタンス</param>
+        /// <returns>
+        ///　削除されたコンポーネントを返します。
+        /// <para>成功または、失敗する可能性があります。</para>
+        /// </returns>
         Result<IComponent> RemoveComponent(IComponent component);
+
+        /// <summary>
+        /// インスタンスからアタッチされているコンポーネントを削除します。
+        /// </summary>
+        /// <param name="component">コンポーネントのインスタンス</param>
+        /// <typeparam name="T"><see cref="IComponent"/>型の型引数</typeparam>
+        /// <returns>
+        ///　削除されたコンポーネントを返します。
+        /// <para>成功または、失敗する可能性があります。</para>
+        /// </returns>
         Result<T> RemoveComponentUnsafe<T>(T component) where T : IComponent;
+
+        /// <summary>
+        /// ジェネリクス型情報からアタッチされているコンポーネントを削除します。
+        /// </summary>
+        /// <typeparam name="T"><see cref="IComponent"/>型の型引数</typeparam>
+        /// <returns>
+        ///　削除されたコンポーネントを返します。
+        /// <para>成功または、失敗する可能性があります。</para>
+        /// </returns>
         Result<T> RemoveComponentUnsafe<T>() where T : IComponent;
+
+        /// <summary>
+        /// ジェネリクス型情報とインスタンスID からアタッチされているコンポーネントを削除します。
+        /// </summary>
+        /// <param name="hash">インスタンスID</param>
+        /// <typeparam name="T"><see cref="IComponent"/>型の型引数</typeparam>
+        /// <returns>
+        ///　削除されたコンポーネントを返します。
+        /// <para>成功または、失敗する可能性があります。</para>
+        /// </returns>
         Result<T> RemoveComponentUnsafe<T>(Guid hash) where T : IComponent;
+
+        /// <summary>
+        /// 型情報からアタッチされているコンポーネントを削除します。
+        /// </summary>
+        /// <param name="type">型情報</param>
+        /// <returns>
+        ///　削除されたコンポーネントを返します。
+        /// <para>成功または、失敗する可能性があります。</para>
+        /// </returns>
         Result<IComponent> RemoveComponentUnsafe(Type type);
+
+        /// <summary>
+        /// 型情報とインスタンスID からアタッチされているコンポーネントを削除します。
+        /// </summary>
+        /// <param name="type">型情報</param>
+        /// <param name="hash">インスタンスID</param>
+        /// <returns>
+        ///　削除されたコンポーネントを返します。
+        /// <para>成功または、失敗する可能性があります。</para>
+        /// </returns>
         Result<IComponent> RemoveComponentUnsafe(Type type, Guid hash);
     }
 }
