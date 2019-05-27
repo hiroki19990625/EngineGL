f77b947fdbf99168c173f82c3730991fdbd929da
hiroki19990625 <hiroki19990625@gmail.com>
2019-05-28 01:29:24 +0900

    ドキュメントの更新

--- EngineGL/Core/ITransform.cs
+++ EngineGL/Core/ITransform.cs
@@ -3,11 +3,29 @@ using EngineGL.Structs.Math;
 
 namespace EngineGL.Core
 {
+    /// <summary>
+    /// オブジェクトの空間座標と、回転、スケールを実装します。
+    /// </summary>
     public interface ITransform
     {
+        /// <summary>
+        /// オブジェクトの空間座標
+        /// </summary>
         Vec3 Position { get; set; }
+
+        /// <summary>
+        /// オブジェクトの回転
+        /// </summary>
         Vec3 Rotation { get; set; }
+
+        /// <summary>
+        /// オブジェクトの境界
+        /// </summary>
         Vec3 Bounds { get; set; }
+
+        /// <summary>
+        /// オブジェクトのスケール
+        /// </summary>
         Vec3 Scale { get; set; }
     }
 }
