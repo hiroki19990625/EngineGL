9c9e6f4062daa79b1baa37fcc80e17cb958971b5
hiroki19990625 <hiroki19990625@gmail.com>
2019-05-28 15:05:47 +0900

    クラスビューを軽く実装

---
+++ EngineGL.Editor/Core/Parser/CSharpCodeParser.cs
@@ -0,0 +1,104 @@
+﻿using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace EngineGL.Editor.Core.Parser
+{
+    public class CSharpCodeParser
+    {
+        private CompilationUnitSyntax _unit;
+
+        public CSharpCodeParser(CompilationUnitSyntax compilationUnit)
+        {
+            _unit = compilationUnit;
+        }
+
+        public CodeCompileUnit Parse()
+        {
+            CodeCompileUnit unit = new CodeCompileUnit();
+
+            CodeNamespace nameSpace = new CodeNamespace();
+            nameSpace.Comments.Clear();
+
+            nameSpace.Imports.AddRange(GetImports(_unit.Usings));
+
+            CodeNamespace bodyNameSpace = GetNamespace();
+
+            unit.Namespaces.Add(nameSpace);
+            unit.Namespaces.Add(bodyNameSpace);
+
+            return unit;
+        }
+
+        public CodeNamespaceImport[] GetImports(SyntaxList<UsingDirectiveSyntax> usingDirectiveList)
+        {
+            CodeNamespaceImport[] imports = new CodeNamespaceImport[usingDirectiveList.Count];
+            for (int i = 0; i < imports.Length; i++)
+            {
+                imports[i] = new CodeNamespaceImport(usingDirectiveList[i].Name.ToString());
+            }
+
+            return imports;
+        }
+
+        public CodeNamespace GetNamespace()
+        {
+            CodeNamespace codeNamespace = new CodeNamespace("DefaultNamespace");
+            foreach (MemberDeclarationSyntax member in _unit.Members)
+            {
+                if (member is NamespaceDeclarationSyntax namespaceDeclaration)
+                {
+                    codeNamespace.Name = namespaceDeclaration.Name.ToString();
+                    codeNamespace.Imports.AddRange(GetImports(namespaceDeclaration.Usings));
+                    codeNamespace.Types.AddRange(GetTypes(namespaceDeclaration.Members));
+                }
+            }
+
+            return codeNamespace;
+        }
+
+        public CodeTypeDeclaration[] GetTypes(SyntaxList<MemberDeclarationSyntax> typeDeclarationList)
+        {
+            List<CodeTypeDeclaration> classes = new List<CodeTypeDeclaration>();
+            for (int i = 0; i < typeDeclarationList.Count; i++)
+            {
+                MemberDeclarationSyntax syntax = typeDeclarationList[i];
+                CodeTypeDeclaration type = new CodeTypeDeclaration();
+                if (syntax is ClassDeclarationSyntax classDeclaration)
+                {
+                    type.Name = classDeclaration.Identifier.Text;
+                    classes.Add(type);
+                }
+                else if (syntax is StructDeclarationSyntax structDeclaration)
+                {
+                    type.Name = structDeclaration.Identifier.Text;
+                    type.IsStruct = true;
+                    classes.Add(type);
+                }
+                else if (syntax is InterfaceDeclarationSyntax interfaceDeclaration)
+                {
+                    type.Name = interfaceDeclaration.Identifier.Text;
+                    type.IsInterface = true;
+                    classes.Add(type);
+                }
+                else if (syntax is EnumDeclarationSyntax enumDeclaration)
+                {
+                    type.Name = enumDeclaration.Identifier.Text;
+                    type.IsEnum = true;
+                    classes.Add(type);
+                }
+                else
+                {
+                    MessageBox.Show(syntax.GetType().Name);
+                }
+            }
+
+            return classes.ToArray();
+        }
+    }
+}
---
+++ EngineGL.Editor/Impl/Controls/Window/CodeParseTreeContent.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EngineGL.Editor.Core.Control.Window;
+using EngineGL.Editor.Core.Parser;
+using Microsoft.CodeAnalysis.CSharp;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace EngineGL.Editor.Impl.Controls.Window
+{
+    class CodeParseTreeContent : MyDockContent
+    {
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+
+        private CSharpCodeParser _parser;
+
+        public CodeParseTreeContent(IMainWindow hostWindow) : base(hostWindow)
+        {
+            InitializeComponent();
+
+            Show(hostWindow.DockPanel, DockState.DockRight);
+        }
+
+        public void OpenFile(string filePath)
+        {
+            string str = File.ReadAllText(filePath, Encoding.UTF8);
+            CSharpSyntaxTree syntax = (CSharpSyntaxTree) CSharpSyntaxTree.ParseText(str);
+
+            _parser = new CSharpCodeParser(syntax.GetCompilationUnitRoot());
+            CodeCompileUnit unit = _parser.Parse();
+            if (unit == null)
+                return;
+
+            CodeNamespace unitNamespace = unit.Namespaces[0];
+            TreeNode importNode = treeView1.Nodes.Add("Imports");
+            foreach (CodeNamespaceImport import in unitNamespace.Imports)
+            {
+                importNode.Nodes.Add(import.Namespace);
+            }
+
+            CodeNamespace body = unitNamespace = unit.Namespaces[1];
+            TreeNode cls = treeView1.Nodes.Add(body.Name);
+            foreach (CodeTypeDeclaration declaration in body.Types)
+            {
+                cls.Nodes.Add(declaration.Name);
+            }
+        }
+
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(282, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 25);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(282, 228);
+            this.treeView1.TabIndex = 2;
+            // 
+            // CodeParseTreeContent
+            // 
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "CodeParseTreeContent";
+            this.Text = "Code Parse Tree";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
