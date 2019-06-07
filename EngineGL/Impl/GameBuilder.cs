abdade2a9444433c309aecc1e048828456af3e06
Kai <aria7716@gmail.com>
2019-06-07 23:06:47 +0900

    build関数追加

--- EngineGL/Impl/GameBuilder.cs
+++ EngineGL/Impl/GameBuilder.cs
@@ -12,29 +12,34 @@ namespace EngineGL.FormatMessage
             _game = new Game();
         }
 
-        public GameBuilder setTitle(string title)
+        public GameBuilder SetTitle(string title)
         {
             _game.Title = title;
             return this;
         }
 
-        public GameBuilder setExceptinDialog(bool exceptionDialog)
+        public GameBuilder SetExceptinDialog(bool exceptionDialog)
         {
             _game.ExceptionDialog = exceptionDialog;
             return this;
         }
 
-        public GameBuilder setDebugLogging(bool debugLogging)
+        public GameBuilder SetDebugLogging(bool debugLogging)
         {
             _game.DebugLogging = debugLogging;
             return this;
         }
 
 
-        public GameBuilder setLoggingConfiguration(LoggingConfiguration loggingConfiguration)
+        public GameBuilder SetLoggingConfiguration(LoggingConfiguration loggingConfiguration)
         {
             _game.LoggingConfiguration = loggingConfiguration;
             return this;
+        }
+
+        public Game Build()
+        {
+            return _game;
         }
     }
 }
