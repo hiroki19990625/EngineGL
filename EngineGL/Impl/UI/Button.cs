10161ce83f88ee76a6cc6d01b827110c56c64f85
Kai <aria7716@gmail.com>
2019-06-07 23:39:00 +0900

    Button,Labelプロパティ定義

--- EngineGL/Impl/UI/Button.cs
+++ EngineGL/Impl/UI/Button.cs
@@ -8,7 +8,7 @@ namespace EngineGL.Impl.UI
     {
         public string Text { get; set; }
         public Colour4 BackColor { get; set; }
-        public Font font { get; set; }
+        public Font Font { get; set; }
         public int FontSize { get; set; }
         public Colour4 FontColor { get; set; }
         public int Width { get; set; }
