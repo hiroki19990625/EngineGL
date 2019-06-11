6b3435a525c6a1793e13e726963a9e9977764d10
elipmoc <relipmoc101@gmail.com>
2019-06-11 23:34:06 +0900

    Transformのtest追加

---
+++ EngineGL.Tests/Impl/Componets/TransformTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using EngineGL.Impl;
+using EngineGL.Structs.Math;
+using NUnit.Framework;
+
+namespace EngineGL.Tests.Impl.Componets
+{
+    [TestFixture]
+    public class TransformTest
+    {
+        [Test]
+        public void TransformTest1()
+        {
+            var obj = new GameObject();
+            Assert.AreEqual(obj.Transform, obj.Transform.GameObject.Transform.GameObject.Transform);
+            var pos = new Vec3(4, 8, 7);
+            obj.SetPosition(pos);
+            Assert.AreEqual(obj.Transform, obj.Transform.GameObject.Transform);
+            Assert.AreEqual(pos, obj.Transform.GameObject.Transform.Position);
+        }
+    }
+}
