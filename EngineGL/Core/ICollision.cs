3f7f071e9242354787d4c30d0ad3803f60aecfd3
Kai <aria7716@gmail.com>
2019-06-14 01:28:20 +0900

    collision実装した

--- EngineGL/Core/ICollision.cs
+++ EngineGL/Core/ICollision.cs
@@ -1,26 +1,10 @@
+using EngineGL.Structs.Math;
+
 namespace EngineGL.Core
 {
-    /// <summary>
-    /// ゲームオブジェクト同士の当たり判定を実装します。
-    /// </summary>
-    public interface ICollision : IComponent
+    public interface ICollision
     {
-        /// <summary>
-        /// ゲームオブジェクト同士接触した時に呼び出されるメソッド
-        /// </summary>
-        /// <param name="gameObject">接触したオブジェクト</param>
-        void OnCollisionEnter(IGameObject gameObject);
-
-        /// <summary>
-        /// ゲームオブジェクト同士が接触している時に呼び出されるメソッド
-        /// </summary>
-        /// <param name="gameObject">接触しているオブジェクト</param>
-        void OnCollisionStay(IGameObject gameObject);
-
-        /// <summary>
-        /// ゲームオブジェクト同士が離れた時に呼び出されるメソッド
-        /// </summary>
-        /// <param name="gameObject">離れたオブジェクト</param>
-        void OnCollisionLeave(IGameObject gameObject);
+        Vec3 Bounds { get; set; }
+        Vec3 Offset { get; set; }
     }
 }
--- EngineGL/Impl/Components/Collision3D.cs
+++ EngineGL/Impl/Components/Collision3D.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections.Concurrent;
 using EngineGL.Core;
 using EngineGL.Structs.Math;
 
@@ -7,71 +5,11 @@ namespace EngineGL.Impl.Components
 {
     public class Collision3D : Component, ICollision
     {
-        private ConcurrentDictionary<Guid, CollisionData3D> _collisionDatas =
-            new ConcurrentDictionary<Guid, CollisionData3D>();
+        public Vec3 Bounds { get; set; }
+        public Vec3 Offset { get; set; }
 
-        public virtual void OnCollisionEnter(IGameObject gameObject)
+        public Collision3D()
         {
-        }
-
-        public virtual void OnCollisionStay(IGameObject gameObject)
-        {
-        }
-
-        public virtual void OnCollisionLeave(IGameObject gameObject)
-        {
-        }
-
-        public override void OnUpdate(double deltaTime)
-        {
-            IScene scene = GameObject.Scene;
-            IObject[] objects = scene.GetObjects().Value;
-            Vec3 obj1 = GameObject.Transform.Position;
-            Vec3 bound1 = GameObject.Transform.Bounds;
-            for (int i = 0; i < objects.Length; i++)
-            {
-                if (objects[i].GetHashCode() != GameObject.GetHashCode() &&
-                    objects[i] is IGameObject gameObject)
-                {
-                    Vec3 obj2 = gameObject.Transform.Position;
-                    Vec3 bound2 = gameObject.Transform.Bounds;
-                    if (bound1.X <= 0 && bound1.Y <= 0 && bound1.Z <= 0 ||
-                        bound2.X <= 0 && bound2.Y <= 0 && bound2.Z <= 0)
-                        continue;
-
-                    if (obj1.X < obj2.X + bound2.X &&
-                        obj1.X + bound1.X > obj2.X &&
-                        obj1.Y < obj2.Y + bound2.Y &&
-                        obj1.Y + bound1.Y > obj2.Y &&
-                        obj1.Z < obj2.Z + bound2.Z &&
-                        obj1.Z + bound1.Z > obj2.Z)
-                    {
-                        if (_collisionDatas.ContainsKey(gameObject.InstanceGuid))
-                        {
-                            OnCollisionStay(gameObject);
-                        }
-                        else
-                        {
-                            _collisionDatas.TryAdd(gameObject.InstanceGuid, new CollisionData3D
-                            {
-                                Entered = true
-                            });
-                            OnCollisionEnter(gameObject);
-                        }
-                    }
-
-                    if (_collisionDatas.ContainsKey(gameObject.InstanceGuid))
-                    {
-                        _collisionDatas.TryRemove(gameObject.InstanceGuid, out _);
-                        OnCollisionLeave(gameObject);
-                    }
-                }
-            }
-        }
-
-        class CollisionData3D
-        {
-            public bool Entered { get; set; }
         }
     }
 }
