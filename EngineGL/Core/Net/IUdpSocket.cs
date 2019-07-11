52a6c2f7260adc11794cad10cd1603efb5bb1a2a
hiroki19990625 <hiroki19990625@gmail.com>
2019-07-11 16:19:40 +0900

    Socketを改修

--- EngineGL/Core/Net/IUdpSocket.cs
+++ EngineGL/Core/Net/IUdpSocket.cs
@@ -3,11 +3,19 @@ using System.Net;
 
 namespace EngineGL.Core.Net
 {
-    public interface ISocket
+    public interface IUdpSocket
     {
         IPEndPoint EndPoint { get; }
         Action<(IPEndPoint endPoint, byte[] buffer)> OnReceive { set; }
         SocketState State { get; }
+
+        bool Broadcast { get; set; }
+        bool DontFragment { get; set; }
+        bool MulticastLoopback { get; set; }
+        short Ttl { get; set; }
+
+        int SendTimeout { get; set; }
+        int ReceiveTimeout { get; set; }
 
         bool Start();
         bool Stop();
---
+++ EngineGL/Impl/Net/TcpServerSocket.cs
@@ -0,0 +1,8 @@
+using EngineGL.Core.Net;
+
+namespace EngineGL.Impl.Net
+{
+    public class TcpServerSocket : ITcpServerSocket
+    {
+    }
+}
